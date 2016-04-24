e81104dcf0f951a85a957f05898e419509bfd40e
SONIA PUTZEL <sonia.putzel@ogilvy.com>
2016-04-24 14:56:16 -0400

    in fair working condition before I reorganize this whole business

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var controller = new ScrollMagic.Controller({
     	globalSceneOptions: {
-			triggerHook: 'onLeave'
 		}
 	});
 
@@ -14,7 +13,8 @@ document.addEventListener("DOMContentLoaded", function() {
 	// create scene for every slide
 	for (var i=0; i<slides.length; i++) {
 		new ScrollMagic.Scene({
-				triggerElement: slides[i]
+				triggerElement: slides[i],
+				triggerHook: 'onLeave'
 			})
 			.setPin(slides[i])
 			.addIndicators() // add indicators (requires plugin)
@@ -38,26 +38,35 @@ document.addEventListener("DOMContentLoaded", function() {
 	// }
 
 
-	// for (var i=0; i<slides.length; i++) {
-	// 	new ScrollMagic.Scene({
-	// 			triggerElement: halfwayTrigger[i + 1],
-	// 			duration: "50%"
-	// 		})
-	// 		.setTween(slideText[i], 1, {opacity: 1}) // trigger a TweenMax.to tween
-	// 		.addIndicators("halfwayTrigger") // add indicators (requires plugin)
-	// 		.addTo(controller);
-	// }
+	for (var i=0; i<slides.length; i++) {
+		new ScrollMagic.Scene({
+				triggerElement: slides[i],
+				duration: "50%",
+				triggerHook: 0.2
+			})
+			.setTween(slideText[i], 1, {opacity: 1}) // trigger a TweenMax.to tween
+			.addTo(controller);
+	}
 
-	// // create scene for every slide
-	// for (var i=0; i<slides.length - 1; i++) {
-	// 	new ScrollMagic.Scene({
-	// 			triggerElement: slides[i],
-	// 			duration: "100%"
-	// 		})
-	// 		.setTween(slideText[i], 1, {opacity: 0}) // trigger a TweenMax.to tween
-	// 		.addIndicators() // add indicators (requires plugin)
-	// 		.addTo(controller);
-	// }
+	for (var i=0; i<slides.length; i++) {
+		new ScrollMagic.Scene({
+				triggerElement: slides[i + 1],
+				duration: "20%",
+				triggerHook: .3
+			})
+			.setTween(slideText[i], 1, {opacity: 0}) // trigger a TweenMax.to tween
+			.addTo(controller);
+	}
+
+
+	new ScrollMagic.Scene({
+			triggerElement: slides[2],
+			duration: "100%",
+			triggerHook: .1
+		})
+		.setTween(document.querySelector("#laptop"), 1, {left: "80%", width: "65%", top: "80%"}) // trigger a TweenMax.to tween
+		.addTo(controller);
+
 
 
 
@@ -71,14 +80,42 @@ document.addEventListener("DOMContentLoaded", function() {
 	// 				.addTo(controller);
 
 
-	// // build tween
-	// var tween2 = TweenMax.to(".whiten", 0.5, {opacity: 1});
+	// build tween
+	var tween2 = TweenMax.to("#whiten-laptop", 0.5, {opacity: 1});
 
-	// // build scene and set duration to window height
-	// var scene = new ScrollMagic.Scene({triggerElement: slides[0], duration: "80%"})
-	// 				.setTween(tween2)
-	// 				.addIndicators() // add indicators (requires plugin)
-	// 				.addTo(controller);
+	// build scene and set duration to window height
+	var scene2 = new ScrollMagic.Scene({triggerElement: slides[1], duration: "200%", triggerHook: 'onLeave'})
+					.setTween(tween2)
+					.addIndicators() // add indicators (requires plugin)
+					.addTo(controller);
+
+	// build tween
+	var tween3 = TweenMax.to("#whiten-monitor", 0.5, {opacity: 1});
+
+	// build scene and set duration to window height
+	var scene3 = new ScrollMagic.Scene({triggerElement: slides[3], duration: "200%", triggerHook: 'onLeave'})
+					.setTween(tween3)
+					.addIndicators() // add indicators (requires plugin)
+					.addTo(controller);
+
+
+	// build tween
+	var tween4 = TweenMax.to("html", 0.5, {background: "#000000"});
+
+	// build scene and set duration to window height
+	var scene4 = new ScrollMagic.Scene({triggerElement: slides[4], duration: "200%", triggerHook: 'onLeave'})
+					.setTween(tween4)
+					.addIndicators() // add indicators (requires plugin)
+					.addTo(controller);
+
+	// build tween
+	var tween5 = TweenMax.to("#whiten-monitor", 0.5, {background: "#000000"});
+
+	// build scene and set duration to window height
+	var scene5 = new ScrollMagic.Scene({triggerElement: document.querySelector("#step-away-panel"), duration: "100%", triggerHook: onLeave})
+					.setTween(tween5)
+					.addIndicators() // add indicators (requires plugin)
+					.addTo(controller);
 
 
 	// // build tween
