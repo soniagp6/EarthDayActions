45ccf07db1348c2cde3b1245e347016bbd5c7950
SONIA PUTZEL <sonia.putzel@ogilvy.com>
2016-04-25 08:45:17 -0400

    got fish to animate in

--- script.js
+++ script.js
@@ -1,121 +1,147 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var controller = new ScrollMagic.Controller({
-    	globalSceneOptions: {
-		}
-	});
+    var controller = new ScrollMagic.Controller();
 
 	// get all slides
-	var slides = document.querySelectorAll("section.panel");
-	var halfwayTrigger = document.querySelectorAll("section.panel .halfway-trigger");
-	var slideText = document.querySelectorAll("section.panel h3");
+	var pinnedItems = document.querySelectorAll(".pinned");
+	var pinTriggers = document.querySelectorAll(".pin");
+	var textElements = document.querySelectorAll("h3");
+	var textPins = document.querySelectorAll(".textpin");
+	var textFadePins = document.querySelectorAll(".fadetext");
+	var whitenElements = document.querySelectorAll(".whiten");
+	var whitenTriggers = document.querySelectorAll(".whiten-trigger");
+	var internalText = document.querySelectorAll(".internal-text");
+	var internalImage = document.querySelectorAll(".internal-image");
+	var internalTextPins = document.querySelectorAll(".internal-text-pin");
 
 
-	// create scene for every slide
-	for (var i=0; i<slides.length; i++) {
+	// create pin for every item
+	for (var i=0; i<pinnedItems.length; i++) {
 		new ScrollMagic.Scene({
-				triggerElement: slides[i],
-				triggerHook: 'onLeave'
+				triggerElement: pinTriggers[i],
+				triggerHook: .05
 			})
-			.setPin(slides[i])
+			.setPin(pinnedItems[i])
 			.addIndicators() // add indicators (requires plugin)
 			.addTo(controller);
 	}
 
-
-	// var scene1 = new ScrollMagic.Scene({triggerElement: "#step-away-panel"})
-	// 	.setPin("#monitor")
-	// 	.addIndicators() // add indicators (requires plugin)
-	// 	.addTo(controller);
-
-
-	// for (var i=0; i<slides.length; i++) {
-	// 	new ScrollMagic.Scene({
-	// 			triggerElement: slides[i + 1]
-	// 		})
-	// 		.setPin(slideText[i])
-	// 		.addIndicators("slide text") // add indicators (requires plugin)
-	// 		.addTo(controller);
-	// }
-
-
-	for (var i=0; i<slides.length; i++) {
+	for (var i=0; i<textElements.length; i++) {
 		new ScrollMagic.Scene({
-				triggerElement: slides[i],
-				duration: "50%",
-				triggerHook: 0.2
+				triggerElement: textPins[i],
+				duration: "80%",
+				triggerHook: .3
 			})
-			.setTween(slideText[i], 1, {opacity: 1}) // trigger a TweenMax.to tween
+			.setTween(textElements[i], 1, {opacity: 1}) // trigger a TweenMax.to tween
 			.addTo(controller);
 	}
 
-	for (var i=0; i<slides.length; i++) {
+	for (var i=0; i<textElements.length; i++) {
 		new ScrollMagic.Scene({
-				triggerElement: slides[i + 1],
-				duration: "20%",
+				triggerElement: textPins[i],
+				duration: "80%",
 				triggerHook: .3
 			})
-			.setTween(slideText[i], 1, {opacity: 0}) // trigger a TweenMax.to tween
+			.setTween(textElements[i], 1, {opacity: 1}) // trigger a TweenMax.to tween
+			.addTo(controller);
+	}
+
+	for (var i=0; i<internalText.length; i++) {
+		new ScrollMagic.Scene({
+				triggerElement: internalTextPins[i],
+				duration: "80%",
+				triggerHook: .3
+			})
+			.setTween(internalText[i], 1, {opacity: 1}) // trigger a TweenMax.to tween
+			.addTo(controller);
+	}
+
+	for (var i=0; i<internalImage.length; i++) {
+		new ScrollMagic.Scene({
+				triggerElement: document.querySelector(".internal-image-pin"),
+				duration: "80%",
+				triggerHook: .3
+			})
+			.setTween(internalImage[i], 1, {opacity: 1}) // trigger a TweenMax.to tween
+			.addTo(controller);
+	}
+
+	for (var i=0; i<textElements.length - 1; i++) {
+		new ScrollMagic.Scene({
+				triggerElement: textFadePins[i], 
+				duration: "30%",
+				triggerHook: 0.3
+			})
+			.setTween(textElements[i], 1, {opacity: 0}) // trigger a TweenMax.to tween
 			.addTo(controller);
 	}
 
 
 	new ScrollMagic.Scene({
-			triggerElement: slides[2],
+			triggerElement: document.querySelector(".shrink-laptop"),
 			duration: "100%",
 			triggerHook: .1
 		})
-		.setTween(document.querySelector("#laptop"), 1, {left: "80%", width: "65%", top: "80%"}) // trigger a TweenMax.to tween
+		.setTween(document.querySelector(".laptop-svg"), 1, {top: "+=60%", width: "30%", right: "-35%"}) // trigger a TweenMax.to tween
 		.addTo(controller);
 
 
 
+	for (var i=0; i<whitenElements.length; i++) {
+		new ScrollMagic.Scene({
+				triggerElement: whitenTriggers[i], 
+				duration: "200%",
+				triggerHook: 0.3
+			})
+			.setTween(whitenElements[i], 1, {opacity: 1}) // trigger a TweenMax.to tween
+			.addTo(controller);
+	}
+
+	new ScrollMagic.Scene({
+			triggerElement: document.querySelector(".shrink-monitor"),
+			duration: "100%",
+			triggerHook: .1
+		})
+		.setTween(document.querySelector("#monitor-svg"), 1, {top: "+=55%", width: "40%"}) // trigger a TweenMax.to tween
+		.addTo(controller);
+
+
+	for (var i=0; i<whitenElements.length; i++) {
+		new ScrollMagic.Scene({
+				triggerElement: document.querySelector("#darken-trigger"), 
+				duration: "100%",
+				triggerHook: 0.3
+			})
+			.setTween(whitenElements[i], 1, {fill: "#000000"}) // trigger a TweenMax.to tween
+			.addTo(controller);
+	}
 
 	// // build tween
-	// var tween1 = TweenMax.to("#laptop", 0.5, {scale: 0.3, top: "-8%"});
+	// var tween3 = TweenMax.to("#whiten-monitor", 0.5, {opacity: 1});
 
 	// // build scene and set duration to window height
-	// var scene = new ScrollMagic.Scene({triggerElement: slides[2], duration: "10%"})
-	// 				.setTween(tween1)
+	// var scene3 = new ScrollMagic.Scene({triggerElement: slides[3], duration: "200%", triggerHook: 'onLeave'})
+	// 				.setTween(tween3)
 	// 				.addIndicators() // add indicators (requires plugin)
 	// 				.addTo(controller);
 
 
-	// build tween
-	var tween2 = TweenMax.to("#whiten-laptop", 0.5, {opacity: 1});
+	// // build tween
+	// var tween4 = TweenMax.to("html", 0.5, {background: "#000000"});
 
-	// build scene and set duration to window height
-	var scene2 = new ScrollMagic.Scene({triggerElement: slides[1], duration: "200%", triggerHook: 'onLeave'})
-					.setTween(tween2)
-					.addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
+	// // build scene and set duration to window height
+	// var scene4 = new ScrollMagic.Scene({triggerElement: slides[4], duration: "200%", triggerHook: 'onLeave'})
+	// 				.setTween(tween4)
+	// 				.addIndicators() // add indicators (requires plugin)
+	// 				.addTo(controller);
 
-	// build tween
-	var tween3 = TweenMax.to("#whiten-monitor", 0.5, {opacity: 1});
+	// // build tween
+	// var tween5 = TweenMax.to("#whiten-monitor", 0.5, {background: "#000000"});
 
-	// build scene and set duration to window height
-	var scene3 = new ScrollMagic.Scene({triggerElement: slides[3], duration: "200%", triggerHook: 'onLeave'})
-					.setTween(tween3)
-					.addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
-
-
-	// build tween
-	var tween4 = TweenMax.to("html", 0.5, {background: "#000000"});
-
-	// build scene and set duration to window height
-	var scene4 = new ScrollMagic.Scene({triggerElement: slides[4], duration: "200%", triggerHook: 'onLeave'})
-					.setTween(tween4)
-					.addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
-
-	// build tween
-	var tween5 = TweenMax.to("#whiten-monitor", 0.5, {background: "#000000"});
-
-	// build scene and set duration to window height
-	var scene5 = new ScrollMagic.Scene({triggerElement: document.querySelector("#step-away-panel"), duration: "100%", triggerHook: onLeave})
-					.setTween(tween5)
-					.addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
+	// // build scene and set duration to window height
+	// var scene5 = new ScrollMagic.Scene({triggerElement: document.querySelector("#step-away-panel"), duration: "100%", triggerHook: onLeave})
+	// 				.setTween(tween5)
+	// 				.addIndicators() // add indicators (requires plugin)
+	// 				.addTo(controller);
 
 
 	// // build tween
